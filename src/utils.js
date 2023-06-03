b81cbbc3d51c92c8bfad379b3c771ee2ea266035
TryCatchB <pozdrakovzena@gmail.com>
2023-06-03 17:09:19 +0300

    JS-constructor

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,7 @@ export function css(styles = {}) {
   if (typeof styles === "string") {
     return styles;
   }
+
   const toString = (key) => `${key}: ${styles[key]}`;
 
   return Object.keys(styles).map(toString).join(";");
